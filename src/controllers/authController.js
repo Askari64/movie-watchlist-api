40972730fccdc170a27b60ef67df282365b55b3c
Askari <askarir64@gmail.com>
2026-02-12 03:00:21 +0530

    initial demo user check
    using clean pattern

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,35 @@
-const signup = (req, res) => {
-    const {name, email, password} = req.body
-  res.json({ message: "🟢 User signup successful", name: name, email: email, password: password});
+import { prisma } from "../config/db.js";
+
+const signup = async (req, res) => {
+  const { name, email, password } = req.body;
+
+  //Check if user already exists
+
+  const userExists = await checkUserExists(email);
+  if (userExists) {
+    return userExistsReponse(res, email);
+  }
+
+  res.json({
+    message: "🟢 User signup successful",
+    name: name,
+    email: email,
+    password: password,
+  });
 };
 
 export { signup };
+
+//********** Functions **********
+
+const checkUserExists = (email) =>
+  prisma.user.findUnique({
+    where: { email: email },
+  });
+
+//********** Responses **********
+
+const userExistsReponse = (res, email) =>
+  res.status(400).json({
+    message: `User with email: ${email} already exists`,
+  });
