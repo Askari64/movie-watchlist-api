86acaa7233b5c03d9a58d5a90521d4608b8e9929
Askari <askarir64@gmail.com>
2026-02-12 21:09:36 +0530

    + Safety check and refactor

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -2,40 +2,31 @@ import { prisma } from "../config/db.js";
 import bcrypt from "bcryptjs";
 
 const signup = async (req, res) => {
-  const { name, email, password } = req.body;
+  try {
+    const { name, email, password } = req.body;
 
-  //Check if user already exists
+    //Check if user already exists
 
-  const userExists = await checkUserExists(email);
-  if (userExists) {
-    return userExistsReponse(res, email);
+    const userExists = await checkUserExists(email);
+    if (userExists) {
+      return userExistsReponse(res, email);
+    }
+
+    // Hash Password
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    // Create User
+
+    const user = await createUser(name, email, hashedPassword);
+    return createUserResponse(res, user);
+  } catch (error) {
+    return res.status(500).json({
+      status: "Error",
+      message: "Internal Server Error - Unable to Signup at the moment",
+    });
   }
-
-  // Hash Password
-
-  const salt = await bcrypt.genSalt(10);
-  const hashedPassword = await bcrypt.hash(password, salt);
-
-  // Create User
-
-  const user = await prisma.user.create({
-    data: {
-      name,
-      email,
-      password: hashedPassword,
-    },
-  });
-
-  res.status(201).json({
-    status: "success",
-    data: {
-      user: {
-        id: user.id,
-        name: name,
-        email: email,
-      },
-    },
-  });
 };
 
 export { signup };
@@ -47,9 +38,29 @@ const checkUserExists = (email) =>
     where: { email: email },
   });
 
+const createUser = (name, email, hashedPassword) =>
+  prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
 //********** Responses **********
 
 const userExistsReponse = (res, email) =>
   res.status(400).json({
     message: `User with email: ${email} already exists`,
+  });
+
+const createUserResponse = (res, user) =>
+  res.status(201).json({
+    status: "success",
+    data: {
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+      },
+    },
   });
