181f74f356ef4e8917b6f7874544e8e4c3d76042
Askari <askarir64@gmail.com>
2026-02-19 02:55:30 +0530

    auth middleware complete

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -2,9 +2,40 @@ import jwt from "jsonwebtoken";
 import { prisma } from "../config/db.js";
 
 const authMiddleware = async (req, res, next) => {
-    console.log('Auth middleware reached')
+  let token;
 
+  //Read token from request
+  const authHeader = req.headers.authorization;
+  const authCookie = req.cookies.jwtAccessToken;
+
+  token =
+    (authHeader?.startsWith("Bearer") ? authHeader?.split(" ")[1] : null) ??
+    authCookie;
+
+  if (!token) {
+    return res.status(401).json({ error: "Unauthorised: Invalid or No token" });
+  }
+
+  //Verify token validity and if user exists
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    const user = await prisma.user.findUnique({
+      where: { id: decoded.id },
+    });
+
+    if (!user) {
+      return res.status(401).json({ error: "User does not exist" });
+    }
+
+
+    req.user = user.id
     next()
+
+  } catch (error) {
+    res.status(401).json({error: 'Not Authorised'})
+  }
 };
 
 export { authMiddleware };
