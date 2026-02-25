ce499e52dcdf47e1823e65c6d26a6256f3b8edaa
Askari <askarir64@gmail.com>
2026-02-25 06:20:25 +0530

    data validation with zod

--- src/routes/watchlistRoutes.js
+++ src/routes/watchlistRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 import { addToWatchlist, deleteFromWatchlist, updateWatchlistItem } from "../controllers/watchlistController.js";
 import { authMiddleware } from "../middleware/authMiddleware.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { addToWatchlistSchema } from "../validators/watchlistValidator.js";
 
 const router = express.Router();
 
 router.use(authMiddleware)
 
-router.post("/",addToWatchlist )
+router.post("/",validateRequest(addToWatchlistSchema), addToWatchlist )
 
 router.put("/:id", updateWatchlistItem)
 
