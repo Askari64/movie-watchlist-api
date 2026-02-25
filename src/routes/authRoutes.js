4a00edbdd8c1bdf86ac71401180f719a64254f83
Askari <askarir64@gmail.com>
2026-02-25 06:28:38 +0530

    authentication data validation

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import express from "express";
 import { signup, login, logout } from "../controllers/authController.js";
+import { validateRequest } from "../middleware/validateRequest.js";
+import { loginSchema, signupSchema } from "../validators/authValidator.js";
 
 const router = express.Router();
 
-router.post("/signup", signup);
-router.post("/login", login);
+router.post("/signup", validateRequest(signupSchema), signup);
+router.post("/login", validateRequest(loginSchema), login);
 router.post("/logout", logout);
 
 export default router;
