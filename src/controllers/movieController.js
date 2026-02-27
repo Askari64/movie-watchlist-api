6eff10fe603d5fb9da5e5b49538fb56d605799a5
Askari <askarir64@gmail.com>
2026-02-27 07:19:21 +0530

    initial commit WIP

---
+++ src/controllers/movieController.js
@@ -0,0 +1,15 @@
+import { prisma } from "../config/db";
+
+const getMovies = (req, res) => {};
+
+const addMovie = (req, res) => {};
+
+const updateMovie = (req, res) => {};
+
+const deleteMovie = (req, res) => {};
+
+export { getMovies, addMovie, updateMovie, deleteMovie };
+
+// Functions
+
+// Responses
