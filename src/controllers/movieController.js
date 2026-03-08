8c19f5d1117063a20404081104f4b028e882a07a
Askari <askarir64@gmail.com>
2026-03-08 08:05:25 +0530

    offset-based pagination in getMovies method

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -1,12 +1,22 @@
-import { prisma } from "../config/db";
+import { prisma } from "../config/db.js";
 
 const getMovies = async (req, res) => {
+  try {
+    //offset-based pagination
+    const page = parseInt(req.query.page) || 1;
+    const limit = parseInt(req.query.limit) || 10;
+    const skip = (page - 1) * limit;
 
-    //TODO: Implement offset-based pagination
+    const [movies, totalMovies] = await findAllMoviesAndCount(skip, limit);
 
-    
-    //List all movies
-    const allMovies = await prisma.movie.findMany()
+    if (totalMovies <= 0) {
+      return noMoviesFoundResponse(res);
+    }
+
+    getAllMoviesSuccessResoponse(res, movies, totalMovies, page, limit);
+  } catch (error) {
+    return getAllMoviesErrorResponse(res);
+  }
 };
 
 const addMovie = async (req, res) => {};
@@ -19,4 +29,38 @@ export { getMovies, addMovie, updateMovie, deleteMovie };
 
 // Functions
 
+const findAllMoviesAndCount = (skip, limit) =>
+  prisma.$transaction([
+    prisma.movie.findMany({
+      skip,
+      take: limit,
+      orderBy: { createdAt: "asc" },
+    }),
+    prisma.movie.count(),
+  ]);
+
 // Responses
+
+const getAllMoviesSuccessResoponse = (res, movies, totalMovies, page, limit) =>
+  res.status(200).json({
+    status: "success",
+    data: movies,
+    meta: {
+      totalMovies,
+      page,
+      limit,
+      totalPages: Math.ceil(totalMovies / limit),
+    },
+  });
+
+const noMoviesFoundResponse = (res) =>
+  res.status(204).json({
+    stats: "success",
+    message: "No Movie Found in Database",
+  });
+
+const getAllMoviesErrorResponse = (res) =>
+  res.status(500).json({
+    status: "error",
+    message: "Server Error - Can not fetch Movies",
+  });
