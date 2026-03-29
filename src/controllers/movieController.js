8ed9507ea50fd6ccd8fbde750acf4f2fe3e48769
Askari <askarir64@gmail.com>
2026-03-30 03:49:22 +0530

    increased pagination default limit to 50

--- src/controllers/movieController.js
+++ src/controllers/movieController.js
@@ -4,7 +4,7 @@ const getMovies = async (req, res) => {
   try {
     //offset-based pagination
     const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+    const limit = parseInt(req.query.limit) || 50;
     const skip = (page - 1) * limit;
 
     const [movies, totalMovies] = await findAllMoviesAndCount(skip, limit);
